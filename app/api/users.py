8e5fede6599cc51d033dae073d266ab777dee888
vvicente-qe <venmar.vicente@3cloudsolutions.com>
2025-08-27 16:44:53 +0800

    delete files and code unrelated to the rest service and tests

--- app/api/users.py
+++ app/api/users.py
@@ -1,9 +1,8 @@
 import sqlalchemy as sa
-from flask import request, url_for, abort, jsonify
+from flask import request, url_for, jsonify
 from app import db
 from app.models import User
 from app.api import bp
-from app.api.auth import token_auth
 from app.api.errors import bad_request
 import json
 
@@ -16,14 +15,12 @@ def json_response(data, status=200, headers=None):
 
 
 @bp.route('/users/<int:id>', methods=['GET'])
-# @token_auth.login_required
 def get_user(id):
     result = db.get_or_404(User, id).to_dict()
     db.session.close()
     return json_response(result, 200)
 
 @bp.route('/users', methods=['GET'])
-# @token_auth.login_required
 def get_users():
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', 10, type=int), 100)
@@ -52,10 +49,7 @@ def create_user():
     return json_response(user.to_dict(), 201, headers)
 
 @bp.route('/users/<int:id>', methods=['PUT'])
-# @token_auth.login_required
 def update_user(id):
-    # if token_auth.current_user().id != id:
-    #     abort(403)
     user = db.get_or_404(User, id)
     data = request.get_json()
     if 'username' in data and data['username'] != user.username and \
@@ -73,7 +67,6 @@ def update_user(id):
     return json_response(result, 200)
 
 @bp.route('/users/<int:id>', methods=['DELETE'])
-# @token_auth.login_required
 def delete_user(id):
     user = db.get_or_404(User, id)
     db.session.delete(user)
--- app/models.py
+++ app/models.py
@@ -5,10 +5,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db, login
-from flask import url_for
-from datetime import timedelta
-import secrets
+from app import db
 
 class PaginatedAPIMixin(object):
     @staticmethod
@@ -26,14 +23,6 @@ class PaginatedAPIMixin(object):
         }
         return data
     
-followers = sa.Table(
-    'followers',
-    db.metadata,
-    sa.Column('follower_id', sa.Integer, sa.ForeignKey('user.id'),
-              primary_key=True),
-    sa.Column('followed_id', sa.Integer, sa.ForeignKey('user.id'),
-              primary_key=True)
-)
 
 class User(PaginatedAPIMixin, UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -41,73 +30,23 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
-    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
     created_date: so.Mapped[datetime] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
     modify_date: so.Mapped[datetime] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
-
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(
-        back_populates='author', passive_deletes=True)
-    
-    token: so.Mapped[Optional[str]] = so.mapped_column(
-        sa.String(32), index=True, unique=True)
-    token_expiration: so.Mapped[Optional[datetime]]
-
-    following: so.WriteOnlyMapped['User'] = so.relationship(
-        secondary=followers, primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.followed_id == id),
-        back_populates='followers', passive_deletes=True)
-    followers: so.WriteOnlyMapped['User'] = so.relationship(
-        secondary=followers, primaryjoin=(followers.c.followed_id == id),
-        secondaryjoin=(followers.c.follower_id == id),
-        back_populates='following', passive_deletes=True)
-    
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-    def avatar(self, size):
-        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
     
-    def follow(self, user):
-        if not self.is_following(user):
-            self.following.add(user)
-
-    def unfollow(self, user):
-        if self.is_following(user):
-            self.following.remove(user)
-
-    def is_following(self, user):
-        query = self.following.select().where(User.id == user.id)
-        return db.session.scalar(query) is not None
-
-    def followers_count(self):
-        query = sa.select(sa.func.count()).select_from(
-            self.followers.select().subquery())
-        return db.session.scalar(query)
-
-    def following_count(self):
-        query = sa.select(sa.func.count()).select_from(
-            self.following.select().subquery())
-        return db.session.scalar(query)
-
-    def posts_count(self):
-        query = sa.select(sa.func.count()).select_from(
-            self.posts.select().subquery())
-        return db.session.scalar(query)
-
     def to_dict(self):
-        return {
+        data = {
             'id': self.id,
             'username': self.username,
             'about_me': self.about_me,
             'email': self.email
         }
+        return data
     
     def from_dict(self, data, new_user=False):
         for field in ['username', 'email', 'about_me']:
@@ -119,44 +58,3 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
             dateNow = datetime.now(timezone.utc)
             self.created_date = dateNow
             self.modify_date = dateNow
-
-    def get_token(self, expires_in=3600):
-        now = datetime.now(timezone.utc)
-        if self.token and self.token_expiration.replace(
-                tzinfo=timezone.utc) > now + timedelta(seconds=60):
-            return self.token
-        self.token = secrets.token_hex(16)
-        self.token_expiration = now + timedelta(seconds=expires_in)
-        db.session.add(self)
-        return self.token
-
-    def revoke_token(self):
-        self.token_expiration = datetime.now(timezone.utc) - timedelta(
-            seconds=1)
-    
-    @staticmethod
-    def check_token(token):
-        user = db.session.scalar(sa.select(User).where(User.token == token))
-        if user is None or user.token_expiration.replace(
-                tzinfo=timezone.utc) < datetime.now(timezone.utc):
-            return None
-        return user
-
-@login.user_loader
-def load_user(id):
-    return db.session.get(User, int(id))
-
-
-class Post(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    body: so.Mapped[str] = so.mapped_column(sa.String(140))
-    timestamp: so.Mapped[datetime] = so.mapped_column(
-        index=True, default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
-                                               index=True)
-
-    author: so.Mapped[User] = so.relationship(back_populates='posts')
-
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
-    
